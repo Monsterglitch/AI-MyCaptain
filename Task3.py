e97946c9a8b286138e22305d01c40e66623a616f
pratheek <pratheekmanimaran@gmail.com>
2022-10-10 21:09:54 +0530

    Task3 is done

---
+++ Task3.py
@@ -0,0 +1,9 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+print("OriginalArray: ",list1)
+del list1[1]
+del list1[3]
+print(list1)
+print("OriginalArray: ",list2)
+del list2[2]
+print(list2)
